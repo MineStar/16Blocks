c62261d8ec5dd66ea504e58c4d410f9cc86c0c1a
GeMoschen <x-chris-x@arcor.de>
2012-03-24 21:30:55 +0100

    - remove skin-deletion on /ban

--- src/main/java/de/minestar/sixteenblocks/Commands/cmdBan.java
+++ src/main/java/de/minestar/sixteenblocks/Commands/cmdBan.java
@@ -8,7 +8,6 @@ import de.minestar.minestarlibrary.utils.PlayerUtils;
 import de.minestar.sixteenblocks.Core.Core;
 import de.minestar.sixteenblocks.Core.TextUtils;
 import de.minestar.sixteenblocks.Manager.AreaManager;
-import de.minestar.sixteenblocks.Manager.SkinArea;
 
 public class cmdBan extends AbstractCommand {
 
@@ -40,32 +39,34 @@ public class cmdBan extends AbstractCommand {
                 TextUtils.sendInfo(player, "Player '" + arguments[0] + "' is offline!");
 
             Bukkit.getOfflinePlayer(playerName).setBanned(true);
-            TextUtils.sendSuccess(player, "Player '" + arguments[0] + "' banned, but Skin was not deleted (because the player is offline!)");
+            TextUtils.sendSuccess(player, "Player '" + arguments[0] + "' banned.");
             return;
         }
 
         // BAN PLAYER
         target.setBanned(true);
+        TextUtils.sendSuccess(player, "Player '" + target.getName() + "' banned.");
 
-        SkinArea thisArea = this.areaManager.getExactPlayerArea(target.getName());
-
-        // CHECK: NO AREA FOUND
-        if (thisArea == null) {
-            TextUtils.sendSuccess(player, "Player '" + target.getName() + "' banned. This player did not have a skin.");
-            target.kickPlayer("You were BANNED!");
-            return;
-        }
-
-        // CHECK : AREA IS NOT BLOCKED
-        if (this.areaManager.isAreaBlocked(thisArea.getZoneXZ())) {
-            TextUtils.sendError(player, "Player '" + target.getName() + "' banned, but Skin was not deleted (blocked by another process!)");
-            target.kickPlayer("You were BANNED!");
-            return;
-        }
-
-        // DELETE THIS AREA
-        this.areaManager.deletePlayerArea(thisArea, player);
-        TextUtils.sendSuccess(player, "Player '" + target.getName() + "' banned and started deletion of '" + thisArea.getAreaOwner() + "'.");
+        /*
+         * SkinArea thisArea =
+         * this.areaManager.getExactPlayerArea(target.getName());
+         * 
+         * // CHECK: NO AREA FOUND if (thisArea == null) {
+         * TextUtils.sendSuccess(player, "Player '" + target.getName() +
+         * "' banned. This player did not have a skin.");
+         * target.kickPlayer("You were BANNED!"); return; }
+         * 
+         * // CHECK : AREA IS NOT BLOCKED if
+         * (this.areaManager.isAreaBlocked(thisArea.getZoneXZ())) {
+         * TextUtils.sendError(player, "Player '" + target.getName() +
+         * "' banned, but Skin was not deleted (blocked by another process!)");
+         * target.kickPlayer("You were BANNED!"); return; }
+         * 
+         * // DELETE THIS AREA this.areaManager.deletePlayerArea(thisArea,
+         * player); TextUtils.sendSuccess(player, "Player '" + target.getName()
+         * + "' banned and started deletion of '" + thisArea.getAreaOwner() +
+         * "'.");
+         */
 
         // KICK PLAYER
         target.kickPlayer("You were BANNED!");
